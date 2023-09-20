379abd102f41919712552c533d844554d5d17221
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-09-20 12:40:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,8 @@ export class App extends Component {
   };
 
   countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
+    const [good, neutral, bad] = this.state;
+    return good + neutral + bad;
   };
 
   countPositiveFeedbackPercentage = () => {
